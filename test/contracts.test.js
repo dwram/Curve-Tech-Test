0b88115f421d8b06e73a3f6e0bcd2634b3092752
Daniel Morris <danielim@hotmail.co.uk>
2024-04-01 00:43:23 +0100

    one more contract creation test for developer safety. even though the model should not be directly used due to a service wrapper

--- test/contracts.test.js
+++ test/contracts.test.js
@@ -9,12 +9,23 @@ after(async () => {
 
 describe("Contract creation", () => {
 
-    it("Should create contract successfully", async () => {
+    it("Should create contract successfully when the required 'name' prerty on the object exists", async () => {
         const newContract = new Contract({ name: 'Contract 1'})
         await newContract.save().then((newContractDocument) => {
             expect(newContractDocument.isNew).to.be.false
             expect(newContractDocument.name).to.equal('Contract 1')
         })
+    })
+
+    it("It should be unsucessful to create a contract when the required 'name' property on the object is missing", async () => {
+        const newContract = new Contract({})
+        try {
+            await newContract.save()
+            expect.fail('Expected ValidationError, but not was thrown')
+        } catch(error) {
+            expect(error.name).to.equal('ValidationError')
+            expect(error.message).to.include("Path `name` is required")
+        }
     })
 
 })
